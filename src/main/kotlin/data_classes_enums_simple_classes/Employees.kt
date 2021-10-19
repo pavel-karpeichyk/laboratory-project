b3d5cb4ee3045a4e825504489be69dc34a357b16
pavel.karpeichyk <avel.karpeichyk@moneyman.ru>
2021-10-19 18:34:43 +0300

    Create two usual classes enum class and data class

---
+++ src/main/kotlin/data_classes_enums_simple_classes/Employees.kt
@@ -0,0 +1,6 @@
+package data_classes_enums_simple_classes
+
+data class Employees(var id: Int, var Name: String, var Surname: String, val profession: Professions) {
+}
+
+
---
+++ src/main/kotlin/data_classes_enums_simple_classes/Professions.kt
@@ -0,0 +1,9 @@
+package data_classes_enums_simple_classes
+
+enum class Professions(salary: Int) {
+  ACCOUNTANT(1000),
+  MANAGER(1000),
+  SALESMAN(800),
+  MARKETER(900),
+  TEAM_LEAD(2000)
+}
