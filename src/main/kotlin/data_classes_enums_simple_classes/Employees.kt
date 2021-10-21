73fb45e4ebb7a5e1f9d1a2a90d76103464bf44cd
pavel.karpeichyk <avel.karpeichyk@moneyman.ru>
2021-10-21 10:34:47 +0300

    Rename enum and data class

--- src/main/kotlin/data_classes_enums_simple_classes/Employees.kt
+++
@@ -1,6 +0,0 @@
-package data_classes_enums_simple_classes
-
-data class Employees(var id: Int, var Name: String, var Surname: String, val profession: Professions) {
-}
-
-
--- src/main/kotlin/data_classes_enums_simple_classes/ProfessionType.kt
+++ src/main/kotlin/data_classes_enums_simple_classes/ProfessionType.kt
@@ -1,6 +1,6 @@
 package data_classes_enums_simple_classes
 
-enum class Professions(salary: Int) {
+enum class ProfessionType(salary: Int) {
   ACCOUNTANT(1000),
   MANAGER(1000),
   SALESMAN(800),
