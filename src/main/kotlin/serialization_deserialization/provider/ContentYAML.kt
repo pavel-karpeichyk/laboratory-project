d1036fcaec6dbda6aa9d231022dba823510947ee
pavel.karpeichyk <avel.karpeichyk@moneyman.ru>
2021-10-25 14:06:08 +0300

    add dependency for YAML file and change in ContentYAML class

--- src/main/kotlin/serialization_deserialization/provider/ContentYAML.kt
+++ src/main/kotlin/serialization_deserialization/provider/ContentYAML.kt
@@ -1,14 +1,14 @@
 package serialization_deserialization.provider
 
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import serialization_deserialization.my_config.MyModelConfiguration
 import java.io.File
 
-class ContentYAML(): ContentFactory {
+class ContentYAML() : ContentFactory {
 
-   override fun getContent( ): MyModelConfiguration {
-      return jacksonObjectMapper().readValue<MyModelConfiguration>(File(FileType.YAML.path))
-
+  override fun getContent(): MyModelConfiguration {
+    return YAMLMapper().readValue<MyModelConfiguration>(File(FileType.YAML.path))
   }
 }
