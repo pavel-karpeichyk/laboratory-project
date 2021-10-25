9e8dfcc1284996caabbac6b0e44402ffa44c9555
pavel.karpeichyk <avel.karpeichyk@moneyman.ru>
2021-10-25 14:10:14 +0300

    add default values in constructor in MyModelConfiguration class

--- src/main/kotlin/serialization_deserialization/my_config/MyModelConfiguration.kt
+++ src/main/kotlin/serialization_deserialization/my_config/MyModelConfiguration.kt
@@ -3,6 +3,6 @@ package serialization_deserialization.my_config
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MyModelConfiguration(var url: String, var login: String, var password: String){
+data class MyModelConfiguration(var url: String="default", var login: String="default", var password: String="default"){
 
 }
