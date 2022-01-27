557c872e19bcd4471d97786009057b5e05b96d8e
pavel.karpeichyk <avel.karpeichyk@moneyman.ru>
2022-01-27 14:22:07 +0300

    correct comments

--- src/main/kotlin/core/watcher/ExecutionConditionCustomAnnotation.kt
+++ src/main/kotlin/core/watcher/ExecutionConditionCustomAnnotation.kt
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.extension.ConditionEvaluationResult
 import org.junit.jupiter.api.extension.ExecutionCondition
 import org.junit.jupiter.api.extension.ExtensionContext
 
-class ValuerExecutionCondition : ExecutionCondition {
+class ExecutionConditionCustomAnnotation : ExecutionCondition {
 
-  override fun evaluateExecutionCondition(context: ExtensionContext?): ConditionEvaluationResult {
-    var result: ConditionEvaluationResult = ConditionEvaluationResult.enabled("Method doesn't  marked my annotation")
+  override fun evaluateExecutionCondition(context: ExtensionContext?): ConditionEvaluationResult? {
+    var result: ConditionEvaluationResult? = null
 
     when (context?.element?.get()?.isAnnotationPresent(Ban::class.java)) {
       true -> result = ConditionEvaluationResult.disabled("Method marked Ban annotation")
+      false -> result = ConditionEvaluationResult.enabled("Method doesn't marked my annotation")
     }
     return result
   }
---
+++ src/test/kotlin/CustomerWatcherTest.kt
@@ -0,0 +1,66 @@
+import core.ui.driver.setter.DriverConfigSetter
+import core.ui.elements.Browser
+import core.watcher.Ban
+import core.watcher.CustomWatcher
+import core.watcher.CustomerWatcherBeforeTest
+import core.watcher.ExecutionConditionCustomAnnotation
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
+import steps.LandingPageSteps
+
+@ExtendWith(
+  CustomWatcher::class,
+  CustomerWatcherBeforeTest::class,
+  ExecutionConditionCustomAnnotation::class
+)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CustomerWatcherTest : BaseTest() {
+
+  private val expectedLoanDays: String = "7"
+  private val expectedAmount: String = "100"
+  private val expectedUrl: String = "http//someurl"
+
+  @Test
+  fun `given false is expected true then test aborted`() {
+    Assumptions.assumeTrue(false)
+  }
+
+  @Test
+  fun `given true is true then test passed`() {
+    assertTrue(true)
+  }
+
+  @Test
+  fun `given false is  true then test failed`() {
+    assertTrue(false)
+  }
+
+  @Disabled
+  @Test
+  fun `given true is true then test disabled`() {
+    assertTrue(true)
+  }
+
+  @Ban
+  @Test
+  fun `given Ban annotation then test disabled`() {
+    assertTrue(true)
+  }
+
+  @Test
+  fun `Submit Landing Page calculator values and verify redirect on Registration Page`() {
+
+    DriverConfigSetter().setDriverConfig()
+    LandingPageSteps().apply {
+      openLandingPage()
+      clickAcceptCoockiesButton()
+      fillCalculatorForm(expectedAmount, expectedLoanDays)
+      clickRequestButton()
+    }
+    Browser.verifyCurrentUrl(expectedUrl)
+  }
+}
