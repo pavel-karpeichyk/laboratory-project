2c69b68f89eaa34ed65f6c7e115468fe123cd3fa
pavel.karpeichyk <avel.karpeichyk@moneyman.ru>
2022-02-09 18:25:41 +0300

    create storage for sql query nd correct comment

--- src/main/kotlin/database_client/SqlQueryBuilder.kt
+++ src/main/kotlin/database_client/SqlQueryBuilder.kt
@@ -5,6 +5,6 @@ import com.vladsch.kotlin.jdbc.SqlQuery
 class SqlQueryBuilder {
 
   fun buildQuery(query: String, param: Map<String, Any>): SqlQuery {
-    return SqlQuery(query, listOf(), param)
+    return SqlQuery(query).params(param)
   }
 }
